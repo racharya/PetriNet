0c01ba4668551e71517d028b707843cf8e6fc20a
Rachana Acharya <rasnacharya@gmail.com>
2018-10-09 16:24:15 -0600

    Add buttons and style them

--- src/components/PetriNetMain.vue
+++ src/components/PetriNetMain.vue
@@ -1,7 +1,45 @@
 <template>
-  <div class="hello">
-    <h1>Hello, World!</h1>
+  <div class="navbar navbar-default navbar-fixed-top">
+    <div class="container-fluid">
+    <h1>Petri Net</h1>
+    <div class="btn-group navbar-btn">
+        <button class="btn btn-secondary btn-sm" id="place-button">
+          <span class="glyphicon glyphicon-plus"></span> Place
+        </button>
+        <button class="btn btn-secondary btn-sm" id="transition-button">
+          <span class="glyphicon glyphicon-plus"></span> Transition
+        </button>
+        <button class="btn btn-secondary btn-sm" id="remove-button">
+          <span class="glyphicon glyphicon-remove"></span> Remove
+        </button>
+        <button class="btn btn-secondary btn-sm" id="clear-button">
+          <span class="glyphicon glyphicon-remove-circle"></span> Clear
+        </button>
+        <button class="btn btn-secondary btn-sm" id="run-button">
+          <span class="glyphicon glyphicon-remove-circle"></span> Run
+        </button>
+        <button class="btn btn-secondary btn-sm" id="stop-button">
+          <span class="glyphicon glyphicon-remove-circle"></span> Stop
+        </button>
+        <button class="btn btn-secondary btn-sm" id="save-button">
+          <span class="glyphicon glyphicon-remove-circle"></span> Save
+        </button>
+        <button class="btn btn-secondary btn-sm" id="restore-button">
+          <span class="glyphicon glyphicon-remove-circle"></span> Restore
+        </button>
+        <button class="btn btn-secondary btn-sm" id="open-button">
+          <span class="glyphicon glyphicon-remove-circle"></span> Open
+        </button>
+        <button class="btn btn-secondary btn-sm" id="download-button">
+          <span class="glyphicon glyphicon-remove-circle"></span> Download
+        </button>
+        <button class="btn btn-secondary btn-sm" id="examples-button">
+          <span class="glyphicon glyphicon-remove-circle"></span> Examples
+        </button>
+      </div>  
+    </div>
   </div>
+
 </template>
 
 <script>
@@ -15,6 +53,16 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+.container-fluid {
+  display: block;
+}
+.btn-primary {
+    border: 1px solid black;
+}
+#run-button, #stop-button {
+  background-color: red;
+
+}
 h3 {
   margin: 40px 0 0;
 }
